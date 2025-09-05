3abe6eefa45e9063ff3876ee34bfc4477e0feabe
Mirudhula <141794395+mirud5173@users.noreply.github.com>
2025-09-05 20:17:49 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/carepredict-ai-compass/" : "/",  // 👈 important
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
+    mode === "development" &&
     componentTagger(),
   ].filter(Boolean),
   resolve: {
